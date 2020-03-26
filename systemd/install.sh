8a6bbd57ffc311493e7fe8a94044438d52c97392
Rumesh <rumesh986@gmail.com>
2020-03-27 00:59:07 +0800

    edited install script to make required files executable

--- systemd/install.sh
+++ systemd/install.sh
@@ -4,4 +4,7 @@ sudo cp ros_boot_start /usr/local/bin/ros_boot_start
 sudo cp ros_boot_stop /usr/local/bin/ros_boot_stop
 sudo cp ros_boot.service /etc/systemd/system/ros_boot.service
 
+sudo chmod +x /usr/local/bin/ros_boot_start
+sudo chmod +x /usr/local/bin/ros_boot_stop
+
 #sudo systemctl enable ros_boot.service
