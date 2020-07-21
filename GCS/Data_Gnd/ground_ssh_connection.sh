f3522eca7397be9234564f37a125bb0312747e50
Lau Yan Han <sps08.lauyanhan@gmail.com>
2020-07-21 17:13:01 +0800

    ground_ssh_connection: Switch from web name to ip address
    This makes it easier to change the server ip address in the future

--- GCS/Data_Gnd/ground_ssh_connection.sh
+++ GCS/Data_Gnd/ground_ssh_connection.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh ubuntu@ec2-18-138-24-228.ap-southeast-1.compute.amazonaws.com -L 4000:localhost:$1 -L 4001:localhost:$2 -R $3:localhost:4002
+ssh ubuntu@18.138.24.228 -L 4000:localhost:$1 -L 4001:localhost:$2 -R $3:localhost:4002
