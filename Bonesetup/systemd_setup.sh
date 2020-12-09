c87dcf34512b9c5863fe3a753811cf3295a1af0e
Rumesh Sudhaharan <rumesh986@gmail.com>
2020-12-09 13:00:59 +0800

    update service to start ogc_airtest.launch

--- Bonesetup/systemd_setup.sh
+++ Bonesetup/systemd_setup.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# sudo cp systemd/ros_boot_start /usr/local/bin/ros_boot_start
-# sudo cp systemd/ros_boot_stop /usr/local/bin/ros_boot_stop
-# sudo cp systemd/ros_boot.service /etc/systemd/system/ros_boot.service
-
-# sudo chmod +x /usr/local/bin/ros_boot_start
-# sudo chmod +x /usr/local/bin/ros_boot_stop
-
-#sudo systemctl enable ros_boot.service
-
 chmod +x systemd/ogc_systemd
 chmod +x systemd/ogc_tmux
 
 sudo cp systemd/ogc_tmux systemd/ogc_systemd /usr/local/bin/
 sudo cp systemd/ogc.service /etc/systemd/system/ogc.service
 
-#sudo systemctl enable ogc.service
+sudo systemctl enable ogc.service
