bc241aafc9e99502da1420da1ecf078cdee1173b
Rumesh Sudhaharan <rumesh986@gmail.com>
2020-12-11 17:28:10 +0800

    enable syncthing after install

--- Bonesetup/systemd_setup.sh
+++ Bonesetup/systemd_setup.sh
@@ -7,3 +7,7 @@ sudo cp systemd/ogc_tmux systemd/ogc_systemd /usr/local/bin/
 sudo cp systemd/ogc.service /etc/systemd/system/ogc.service
 
 sudo systemctl enable ogc.service
+
+# start and enable syncthing 
+sudo systemctl enable syncthing@$(whoami)
+sudo systemctl start syncthing@$(whoami)
